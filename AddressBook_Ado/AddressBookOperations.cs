53cfc30f88a2b948471b5bb23c94ceaa36216b8a
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-31 17:16:14 +0530

    [Nithin]Add : Updation of Creating a DataBase

---
+++ AddressBook_Ado/AddressBookOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AddressBook_Ado
+{
+    public class AddressBookOperations
+    {
+        public void CreateDatabase()
+        {
+            SqlConnection con = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog=master;integrated security = true ");
+            try
+            {
+                string query = "Create Database AddressBookService";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("DataBase Created Sucessfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is no database created ");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ AddressBook_Ado/Program.cs
@@ -0,0 +1,14 @@
+﻿using AddressBook_Ado;
+using Azure;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("! Address_Book !");
+
+        AddressBookOperations operation = new AddressBookOperations();
+        operation.CreateDatabase();
+
+    }
+}
